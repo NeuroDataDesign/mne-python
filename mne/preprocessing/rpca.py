5250bbfdced04045dc16d77b60fda208c37313da
rbechto2 <54995036+rbechto2@users.noreply.github.com>
2019-11-11 13:53:53 -0500

    Update rpca.py

--- mne/preprocessing/rpca.py
+++ mne/preprocessing/rpca.py
@@ -1,57 +1,20 @@
 import numpy as np
 from sklearn.decomposition import TruncatedSVD
 
-def performRPCA(EEG, lam=-1, tol=1e-7, maxIter=1000):
-
-    """ Perform Robust Principle Component Analysis:
-    Performs a Robust Principal Component Analysis on the EEG data with
-    the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
-    The function outputs the EEG data with the noise removed as well as the nosie
-    that was removed.
-    
-    Parameters
-    ----------
-    EEG : double numpy array
-        First Parameter, EEG Data (must include)
-    lam : double (default = 1/(sqrt(# of Colunms))
-        Second Parameter, lamda paramter for RPCA (input -1 for default calculation)
-    tol : double (defalut = 1e-7)
-        Third Parameter, tolerance RPCA parameter
-    maxIter : int
-        Fourth Parameter, Maximum Iterations (deafult = 1000)
-    
-    Returns
-    -------
-    Data : double numpy array
-        Corrected Data
-    Noise : double numpy array
-        Noise removed from the data, Original EEG = Data + Noise        
-    
-    """
-    #Find lamda if not provided using the Automagic algorithim
-    col = EEG.shape;
-    if (lam == -1): #if no input lamda, calculate its value
-        lam = 1 / np.sqrt(col[0]);
-        
-    #Perform Robust Principal Component Analysis
-    data, error = preprocess.rpca(EEG,lam,tol,maxIter);
-    return data, error;
-    
-    """
-    Cian Scannell - Oct-2017 (https://github.com/cianmscannell/RPCA)
-    Computes rpca separation of M into L(low rank) and S(Sparse) using the parameter lam
-    this uses the alternating directions augmented method of multipliers
-    as described in my blog
-"""
 
 def rpca(M,lam,tol,maxIter):
 
     """ Perform Robust Principle Component Analysis:
-        
+    
     Performs a Robust Principal Component Analysis on the EEG data with
     the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
     The function outputs the EEG data with the noise removed as well as the nosie
     that was removed.
+    
+    Adapted from Cian Scannell - Oct-2017 (https://github.com/cianmscannell/RPCA)
+    Computes rpca separation of M into L(low rank) and S(Sparse) using the parameter lam
+    this uses the alternating directions augmented method of multipliers.
+    
     
     parameters
     ----------
@@ -65,14 +28,18 @@ def rpca(M,lam,tol,maxIter):
            fourth parameter, Maximum Iterations (deafult = 1000)
     return
     ------
-        L : npumpy.darray 
+        Data : npumpy.darray 
             Corrected Data (Low rank matrix)
-        S : npumpy.darray 
+        Error : npumpy.darray 
             Noise removed from the data (Sparse Matrix)
             note: M = L + S        
     
 """
-
+    #Calculate lamda if not provided using the Automagic algorithim
+    col = EEG.shape;
+    if (lam == -1): #if no input lamda, calculate its value
+        lam = 1 / np.sqrt(col[0]);
+        
     Nr = M.shape[0]
     Nc = M.shape[1]
     norm_2 = np.linalg.norm(M,2)
@@ -104,10 +71,10 @@ def rpca(M,lam,tol,maxIter):
             isRunning = False;
             
             
-    L = L.reshape(Nr,Nc)
-    S = S.reshape(Nr,Nc)
+    Data = L.reshape(Nr,Nc)
+    Error = S.reshape(Nr,Nc)
 
-    return L,S
+    return Data, Error
 
 def soft_thres(x,eps):
 
